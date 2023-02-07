e4ec46ed677bb918ce5c52cd0818daaeefab2eb3
Pavithra <pavithra.karanth@codecraft.co.in>
2023-02-07 09:16:37 +0530

    Building a weather app that displays the current weather for a given location

--- weather-app/src/components/WeatherApp/Weather.js
+++ weather-app/src/components/WeatherApp/Weather.js
@@ -2,40 +2,24 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import "./Weather.css";
 
-function WeatherApp() {
+export default function WeatherApp() {
   const [weather, setWeather] = useState({});
   const [city, setCity] = useState("");
   const [icon, setIcon] = useState(null);
   const [error, setError] = useState("");
-  const [isLoading, setIsLoading] = useState(false);
   const [time, setTime] = useState("");
 
-
+  
   useEffect(() => {
-    navigator.geolocation.getCurrentPosition(position => {
-      const { latitude, longitude } = position.coords;
-      axios
-        .get(
-          `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=imperial&appid=83dc2e9a8e5ae5be71b3d639664d624c`,
-        )
-        .then((res) => {
-          setWeather(res.data);
-          setIcon(
-            `http://openweathermap.org/img/w/${res.data.weather[0].icon}.png`,
-          );
-        })
-        .catch((err) => {
-          console.log(err);
-          
-        });
+    axios.get( `https://api.openweathermap.org/data/2.5/weather?q=mangalore&units=imperial&appid=83dc2e9a8e5ae5be71b3d639664d624c`).then((res) => {
+      setWeather(res.data);
+      
+      console.log(res.data);
     });
   }, []);
 
-  // To handle submit button and fetch the data.
   function handleSubmit(event) {
     event.preventDefault();
-    setIsLoading(true);
-  if(city) {
     axios
       .get(
         `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=83dc2e9a8e5ae5be71b3d639664d624c`,
@@ -48,38 +32,16 @@ function WeatherApp() {
         );
         setTime(new Date().toLocaleString());
         console.log(res.data);
-        setIsLoading(false);
-        setError("")
       })
       .catch((err) => {
         console.log(err);
-        setIsLoading(false);
         setError("Invalid city name. Please try again");
       });
-  } else {
-    navigator.geolocation.getCurrentPosition(position => {
-      const { latitude, longitude } = position.coords;
-      axios
-        .get(
-          `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=imperial&appid=83dc2e9a8e5ae5be71b3d639664d624c`,
-        )
-        .then((res) => {
-          setWeather(res.data);
-          setIcon(
-            `http://openweathermap.org/img/w/${res.data.weather[0].icon}.png`,
-          );
-        })
-        .catch((err) => {
-          console.log(err);
-        });
-    });
   }
-}
 
   return (
     <div className="container">
-      <h3>WEATHER APP</h3>
-
+      <h3>Weather App</h3>
       <div className="container__search">
         <input
           className="container__input"
@@ -88,33 +50,38 @@ function WeatherApp() {
           placeholder="Enter City Name"
           type="text"
         />
-        <button className="container__button" onClick={handleSubmit} disabled={isLoading}>
+        <button className="container__button" onClick={handleSubmit}>
           Submit
         </button>
       </div>
+
       {error && <p className="container__error">{error}</p>}
 
-
-      <div className="container__weather-data">
-
       <div className="container__time">
-  {city && (
-    <>
-      <p className="container__data">{time}</p>
-    </>
-  )}
-</div>
+        {city && (
+          <>
+            <p className="container__data">{time}</p>
+          </>
+        )}
+      </div>
+      
+      <div className="container__temp">
+        {weather.name ? <h1> Location - {weather.name}</h1> : " "}
+      </div>
 
+     
+      <div className="container__weather-data">
         <div className="container__temp">
           {weather.main ? <h1>Temperature - {weather.main.temp}°F</h1> : " "}
         </div>
-      
-<div className="container__wrap">
+
         {weather.name !== undefined && (
           <div className="container__wind-humidity">
             <div className="container__humidity">
               {weather.main ? (
-                <p className="container__data">Humidity - {weather.main.humidity}%</p>
+                <p className="container__data">
+                  Humidity - {weather.main.humidity}%
+                </p>
               ) : (
                 " "
               )}
@@ -122,7 +89,9 @@ function WeatherApp() {
 
             <div className="container__wind">
               {weather.wind ? (
-                <p className="container__data">Wind Speed - {weather.wind.speed} MPH</p>
+                <p className="container__data">
+                  Wind Speed - {weather.wind.speed} MPH
+                </p>
               ) : (
                 " "
               )}
@@ -131,11 +100,8 @@ function WeatherApp() {
               {icon && <img src={icon} alt="weather" />}
             </div>
           </div>
-          
         )}
-        </div>
       </div>
     </div>
   );
 }
-export default WeatherApp;
